55fec6bffd3f0831ebb7bbe618e3a86ba313012c
Kvj-Harsha <cs23b1034@iiitr.ac.in>
2024-12-15 17:07:22 +0530

    cleanup t1

--- app/_components/Hero.js
+++ app/_components/Hero.js
@@ -1,4 +1,6 @@
 import Button from "./Button";
+import Image from "next/image";
+
 
 export default function Home() {
   return (
@@ -11,19 +13,20 @@ export default function Home() {
         rel="stylesheet"
       />
 
-      <div className="container py-24">
+      <div className="container py-24 relative">
+       
         <div className="flex flex-col items-center justify-center gap-4 text-center">
           <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
             🌿 Fresh Piks 2k25!
           </div>
-          <h1 className="text-5xl sm:text-6xl md:text-7xl font-medium mt-6 mx-auto">
+          <h1 className="text-6xl md:text-6xl  font-medium mt-6 mx-auto">
             Find the Perfect Rent <br /> in a Few Clicks!
           </h1>
           <p className="text-lg sm:text-xl text-white/50 mt-6 p-3 max-w-2xl mx-auto">
             QuickRent helps you find your perfect rental in minutes. Fast, easy,
             and hassle-free – your next home is just a few clicks away!
           </p>
-          <div className="p-4 w-full">
+          <div className="p-4  w-full">
             <form className="flex items-center justify-center border border-white/25 rounded-full p-2 mt-8 max-w-lg w-full mx-auto">
               <input
                 type="email"
